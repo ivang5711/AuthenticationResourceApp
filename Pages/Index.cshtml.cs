803d516726f3bea6d10581b8ffee003b4b44aa2a
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2023-12-14 02:47:56 +0400

    Adds Locked feature

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
 
 namespace AuthFormApp.Pages;
 
+[Authorize]
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
@@ -14,6 +16,21 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        SqliteConnection con = new("DataSource=C:\\Users\\Smith\\source\\repos\\pet_projects\\AuthFormApp\\app.db");
+        con.Open();
+        using var cmd = con.CreateCommand();
+        cmd.CommandText = "SELECT * FROM AspNetUsers;";
+        using var reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            reader.IsDBNull(0);
+            Console.WriteLine($"guid: {reader.GetString(0)} email: {reader.GetString(3)} " +
+                $"LockoutEnabled: {reader.GetInt32(5)}");
+        }
 
+        using var cmd2 = con.CreateCommand();
+        cmd2.CommandText = $"UPDATE AspNetUsers SET LockoutEnd = '{DateTime.MaxValue}' WHERE Email = 'kar@kar.kar';";
+        cmd2.ExecuteNonQuery();
+        
     }
 }
