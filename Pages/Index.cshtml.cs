c0ca5e2b996461d238d81b9c482a12277234db33
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2023-12-15 03:07:08 +0400

    Adds User management rable
    Added user management table.
    Implemented username, email, status fields view.
    Aadded javascript code to manipulate checkboxes.
    Added toolbaar buttons to lock, unlock and delete user.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthFormApp.Pages;
 
@@ -12,6 +14,13 @@ public class IndexModel : PageModel
     private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
     public int MyProperty { get; set; } = 777;
+
+    public SelectList Users { get; set; }
+    public List<string> UserNames { get; set; } = new List<string>();
+    public List<string> UsersEmail { get; set; } = new List<string>();
+    public List<string> UsersStatus { get; set; } = new List<string>();
+    public List<string> UsersLastLogin { get; set; } = new List<string>();  
+    public List<string> UsersRegiastrationTime { get; set; } = new List<string>();  
 
     public IndexModel(ILogger<IndexModel> logger, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
     {
@@ -26,6 +35,16 @@ public class IndexModel : PageModel
         {
             IdentityUser? user = await _userManager.FindByNameAsync(User.Identity!.Name!);
             await _signInManager.RefreshSignInAsync(user!);
+        }
+
+        List<IdentityUser> users = await _userManager.Users.ToListAsync();
+        Users = new SelectList(users, nameof(IdentityUser.UserName));
+
+        foreach (IdentityUser item in users)
+        {
+            UserNames.Add(item.UserName);   
+            UsersEmail.Add(item.Email);
+            UsersStatus.Add(item.LockoutEnd is not null ? "Blocked" : "Active");
         }
 
         return Page();
