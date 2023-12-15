b73d3c6559b284c61c2fb32bf4ab24f7fc2215e0
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2023-12-16 00:23:23 +0400

    Adds checkbox and buttons integration
    Added checkboxs and button integration to table.

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -21,6 +21,9 @@ public class IndexModel : PageModel
     public List<string> UsersLastLogin { get; set; } = new List<string>();
     public List<string> UsersRegiastrationTime { get; set; } = new List<string>();
 
+    [BindProperty]
+    public List<string> MyProperty { get; set; } = new();
+
     public string MyProperty1 { get; set; }
 
     public IndexModel(ILogger<IndexModel> logger, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
@@ -43,6 +46,78 @@ public class IndexModel : PageModel
                 return Redirect("/Index");
             }
         }
+
+        List<IdentityUser> users = await _userManager.Users.ToListAsync();
+        Users = new SelectList(users, nameof(IdentityUser.UserName));
+
+        //var claims = ClaimsPrincipal.Current.Identities.First().Claims.ToList();
+
+        //var nameIdentifier = User.FindFirst(ClaimTypes.Country);
+
+        foreach (IdentityUser item in users)
+        {
+            //UserNames.Add(item.UserName);
+            UsersEmail.Add(item.Email);
+            UsersStatus.Add((item.LockoutEnd is not null || _userManager.IsInRoleAsync(item, "Locked").Result) ? "Blocked" : "Active");
+
+            var existingUserClaims = await _userManager.GetClaimsAsync(item);
+
+            foreach (var claim in existingUserClaims)
+            {
+                if (claim.Type == "RegistrationDateTime")
+                {
+                    UsersRegiastrationTime.Add(claim.Value);
+                    break;
+                }
+            }
+
+            foreach (var claim in existingUserClaims)
+            {
+                if (claim.Type == "LastLogin")
+                {
+                    UsersLastLogin.Add(claim.Value);
+                    break;
+                }
+            }
+
+            foreach (var claim in existingUserClaims)
+            {
+                if (claim.Type == "PersonName")
+                {
+                    UserNames.Add(claim.Value);
+                    break;
+                }
+            }
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            IdentityUser? user = await _userManager.FindByNameAsync(User.Identity!.Name!);
+            await _signInManager.RefreshSignInAsync(user!);
+            var t = user.LockoutEnd;
+
+            if (t == DateTime.MaxValue || _userManager.IsInRoleAsync(user, "Locked").Result)
+            {
+                return Redirect("/Index");
+            }
+        }
+
+
+        var selectedItems = Request.Form["row"].ToList();
+
+        foreach(var item in selectedItems)
+        {
+            if (item is not null)
+            {
+                MyProperty.Add(item);
+            }
+        }
+
 
         List<IdentityUser> users = await _userManager.Users.ToListAsync();
         Users = new SelectList(users, nameof(IdentityUser.UserName));
